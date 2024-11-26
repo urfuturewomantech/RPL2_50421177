1232d2c365950a754cdc485fa64cfe64aea70099
urfuturewomantech <126690731+urfuturewomantech@users.noreply.github.com>
2024-11-26 07:02:54 +0700

    Add files via upload

---
+++ pert3_50421177/src/main/java/me/anisa_mahasiswa/pert3_50421177/Pert3_50421177.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package me.anisa_mahasiswa.pert3_50421177;
+
+/**
+ *
+ * @author ASUS
+ */
+public class Pert3_50421177 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
